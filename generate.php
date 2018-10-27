bfcb26d10bd48e2cfd7ce2407e6d6c54dd5b1453
Ken Rickard <agentrickard@gmail.com>
2018-10-27 16:16:06 -0400

    Documents the generate.php file

--- generate.php
+++ generate.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * Playing card generation script.
+ *
+ * This file should be run from the command line. It assumes that your script
+ * shares a directory with two files, list.csv and template-card.svg.
+ *
+ * > php generate.php
+ */
+
 // Load the CSV.
 $csv = get_csv();
 var_dump($csv);
@@ -9,6 +18,9 @@ process_cards($csv);
 
 /**
  * Gets the CSV contents.
+ *
+ * @return array
+ *   An array of content rows, keyed by the header row of the CSV.
  */
 function get_csv() {
   $filename = "list.csv";
@@ -21,19 +33,25 @@ function get_csv() {
   return $csv;
 }
 
+/**
+ * Combines header and content arrays using array_walk().
+ */
 function _combine_array(&$row, $key, $header) {
   $row = array_combine($header, $row);
 }
 
 /**
- * Process the cards.
+ * Processes the cards and exports the files to disk.
+ *
+ * @param array $csv
+ *   The contents of a CSV file, as an array, with a header row.
  */
 function process_cards($csv) {
-  $svg = 'template-card.svg';
-  $content = file_get_contents($svg);
+  $file = 'template-card.svg';
+  $svg = file_get_contents($file);
   foreach ($csv as $row) {
     $name = 'card-' . strtolower($row['name']) . '.svg';
-    $text = replace_content($content, $row);
+    $text = replace_svg($svg, $row);
     file_put_contents($name, $text);
     export_file($name);
   }
@@ -41,9 +59,16 @@ function process_cards($csv) {
 
 /**
  * Data processing for cards.
+ *
+ * @param $svg
+ *   The content defined by an SVG file.
+ * @param $row
+ *   A data row from the CSV file.
+ *
+ * @return
+ *   The text for an SVG file.
  */
-function replace_content($content, $row) {
-  $text = $content;
+function replace_svg($svg, $row) {
   $find = [
     'Placeholder Text',
     'Action Text',
@@ -54,12 +79,22 @@ function replace_content($content, $row) {
     $row['text'],
     $row['color'],
   ];
-  $text = str_replace($find, $replace, $content);
+  $text = str_replace($find, $replace, $svg);
   return $text;
 }
 
 /**
- * Exports a file as PNG.
+ * Exports an SVG file as PNG.
+ *
+ * @param $file
+ *   The name of the file being exported.
+ * @param $location
+ *   The directory to export the file to, relative to the script. Defaults to
+ *   'output'.
+ * @param $format
+ *   The output format. Defaults to PNG.
+ * @param int $dpi
+ *   Dots per inch of the export file. Defaults to 300.
  */
 function export_file($file, $location = 'output', $format = 'png', $dpi = 300) {
   $newfile = str_replace('.svg', '.png', $file);
